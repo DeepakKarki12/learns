b93ebaedaef735bbc57a517f7f6da210f2c6286f
DeepakKarki12 <indianarmypara9826@gamil.com>
2024-08-27 21:20:01 +0530

    frontend

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
 app.use('/',(req,res)=>{
     console.log("hello world")
-    res.json({"message":"Hello world"})
+
+    const pathOffile = path.join(__dirname,'frontend/')
+    console.log(pathOffile)
+    return res.sendFile("signup.html",{root : pathOffile})
 })
 
 app.listen(9000,() => {
